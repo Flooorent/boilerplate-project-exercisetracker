f7a530824a27b7d98038ab5ff5afe7db102e4f76
Flooorent <florent.moiny@gmail.com>
2019-04-24 10:45:09 +0200

    use correct urls in tests, add unit tests for GET /api/exercise/log

--- test/app.js
+++ test/app.js
@@ -248,7 +248,7 @@ describe('app', function() {
 
         it('should send an error if userId, description, or duration are not specified', function(done) {
             chai.request(app)
-                .post('/api/exercise/new-user')
+                .post('/api/exercise/add')
                 .send({ description: 'pectorals', duration : 60 })
                 .end(function(err, res) {
                     if(err) {
@@ -266,7 +266,7 @@ describe('app', function() {
 
         it('should send an error if description is not specified', function(done) {
             chai.request(app)
-                .post('/api/exercise/new-user')
+                .post('/api/exercise/add')
                 .send({ userId: '1334', duration : 60 })
                 .end(function(err, res) {
                     if(err) {
@@ -284,7 +284,7 @@ describe('app', function() {
 
         it('should send an error if duration is not specified', function(done) {
             chai.request(app)
-                .post('/api/exercise/new-user')
+                .post('/api/exercise/add')
                 .send({ userId: '123', description: 'pectorals' })
                 .end(function(err, res) {
                     if(err) {
@@ -311,7 +311,7 @@ describe('app', function() {
                     const userId = result.body._id
 
                     chai.request(app)
-                        .post('/api/exercise/new-user')
+                        .post('/api/exercise/add')
                         .send({ userId: `${userId}1`, description: 'pectorals', duration: 60 })
                         .end(function(err, res) {
                             if(err) {
@@ -330,11 +330,219 @@ describe('app', function() {
     })
 
     describe('GET /api/exercise/log?{userId}[&from][&to][&limit]', function() {
-        it('should return the user object with the log and count')
-        it('should handle from optional parameter')
-        it('should handle to optional parameter')
-        it('should handle limit optional parameter')
-        it('should handle all optional parameters at the same time')
+        const firstUsername = 'flo'
+        const secondUsername = 'jean'
+
+        let firstUserId
+        let secondUserId
+
+        const firstUserFullLog = [
+            {
+                description: 'ex1',
+                duration: 20,
+                date: '2019-04-10'
+            },
+            {
+                description: 'ex2',
+                duration: 40,
+                date: '2019-04-12'
+            },
+            {
+                description: 'ex3',
+                duration: 60,
+                date: '2019-04-14'
+            },
+        ]
+
+        const firstUserFullLogCount = firstUserFullLog.length
+
+        beforeEach(function(done) {
+            Promise.all([
+                chai.request(app)
+                    .post('/api/exercise/new-user')
+                    .send({ username: firstUsername })
+                    .end(function(err, res) {
+                        if(err) {
+                            throw new Error(err)
+                        }
+
+                        firstUserId = res.body._id
+                    }),
+                chai.request(app)
+                    .post('/api/exercise/new-user')
+                    .send({ username: secondUsername })
+                    .end(function(err, res) {
+                        if(err) {
+                            throw new Error(err)
+                        }
+
+                        secondUserId = res.body._id
+                    }),
+            ])
+            .then(() => {
+                Promise.all([
+                    chai.request(app).post('/api/exercise/add').send({ userid: firstUserId, ...firstUserFullLog[0] }),
+                    chai.request(app).post('/api/exercise/add').send({ userid: secondUserId, description: 'ex11', duration: 30, date: '2019-04-11' }),
+                    chai.request(app).post('/api/exercise/add').send({ userid: firstUserId, ...firstUserFullLog[1] }),
+                    chai.request(app).post('/api/exercise/add').send({ userid: firstUserId, ...firstUserFullLog[2] }),
+                ])
+                .then(() => done())
+            })
+        })
+
+        it('should return the user object with the log and count', function(done) {
+            chai.request(app)
+                .get(`/api/exercise/log?${firstUserId}`)
+                .end(function(err, res) {
+                    if(err) {
+                        throw new Error(err)
+                    }
+
+                    res.should.have.status(httpStatus.OK)
+                    res.should.be.json
+                    res.body.should.be.an('object')
+                    res.body.should.have.property('_id')
+                    res.body.should.have.property('username')
+                    res.body.should.have.property('log')
+                    res.body.should.have.property('count')
+                    res.body._id.should.equal(firstUserId)
+                    res.body.username.should.equal(firstUsername)
+                    res.body.log.should.be.an('array')
+                    res.body.log.should.deep.equal(firstUserFullLog)
+                    res.body.count.should.equal(firstUserFullLogCount)
+
+                    done()
+                })
+        })
+
+        it('should handle from optional parameter', function(done) {
+            chai.request(app)
+            .get(`/api/exercise/log?${firstUserId}&from=2019-04-12`) // don't retrieve the first exercise
+            .end(function(err, res) {
+                if(err) {
+                    throw new Error(err)
+                }
+
+                res.should.have.status(httpStatus.OK)
+                res.should.be.json
+                res.body.should.be.an('object')
+                res.body.should.have.property('_id')
+                res.body.should.have.property('username')
+                res.body.should.have.property('log')
+                res.body.should.have.property('count')
+                res.body._id.should.equal(firstUserId)
+                res.body.username.should.equal(firstUsername)
+                res.body.log.should.be.an('array')
+                res.body.log.should.deep.equal(firstUserFullLog.slice(1))
+                res.body.count.should.equal(firstUserFullLogCount-1)
+
+                done()
+            })
+        })
+
+        it('should handle to optional parameter', function(done) {
+            chai.request(app)
+            .get(`/api/exercise/log?${firstUserId}&to=2019-04-11`) // don't retrieve the last two exercises
+            .end(function(err, res) {
+                if(err) {
+                    throw new Error(err)
+                }
+
+                res.should.have.status(httpStatus.OK)
+                res.should.be.json
+                res.body.should.be.an('object')
+                res.body.should.have.property('_id')
+                res.body.should.have.property('username')
+                res.body.should.have.property('log')
+                res.body.should.have.property('count')
+                res.body._id.should.equal(firstUserId)
+                res.body.username.should.equal(firstUsername)
+                res.body.log.should.be.an('array')
+                res.body.log.should.deep.equal(firstUserFullLog.slice(0, 1))
+                res.body.count.should.equal(1)
+
+                done()
+            })
+        })
+
+        it('should handle limit optional parameter', function(done) {
+            const limit = 2
+
+            chai.request(app)
+            .get(`/api/exercise/log?${firstUserId}&limit=${limit}`)
+            .end(function(err, res) {
+                if(err) {
+                    throw new Error(err)
+                }
+
+                res.should.have.status(httpStatus.OK)
+                res.should.be.json
+                res.body.should.be.an('object')
+                res.body.should.have.property('_id')
+                res.body.should.have.property('username')
+                res.body.should.have.property('log')
+                res.body.should.have.property('count')
+                res.body._id.should.equal(firstUserId)
+                res.body.username.should.equal(firstUsername)
+                res.body.log.should.be.an('array')
+                res.body.log.should.deep.equal(firstUserFullLog.slice(0, limit))
+                res.body.count.should.equal(limit)
+
+                done()
+            })
+        })
+
+        it('should retrieve all exercises if limit parameter is larger than the number of all exercises', function(done) {
+            const limit = 5
+
+            chai.request(app)
+            .get(`/api/exercise/log?${firstUserId}&limit=${limit}`)
+            .end(function(err, res) {
+                if(err) {
+                    throw new Error(err)
+                }
+
+                res.should.have.status(httpStatus.OK)
+                res.should.be.json
+                res.body.should.be.an('object')
+                res.body.should.have.property('_id')
+                res.body.should.have.property('username')
+                res.body.should.have.property('log')
+                res.body.should.have.property('count')
+                res.body._id.should.equal(firstUserId)
+                res.body.username.should.equal(firstUsername)
+                res.body.log.should.be.an('array')
+                res.body.log.should.deep.equal(firstUserFullLog)
+                res.body.count.should.equal(firstUserFullLogCount)
+
+                done()
+            })
+        })
+
+        it('should handle all optional parameters at the same time', function(done) {
+            chai.request(app)
+            .get(`/api/exercise/log?${firstUserId}&from=2019-04-11&to=2019-04-15&limit=1`)
+            .end(function(err, res) {
+                if(err) {
+                    throw new Error(err)
+                }
+
+                res.should.have.status(httpStatus.OK)
+                res.should.be.json
+                res.body.should.be.an('object')
+                res.body.should.have.property('_id')
+                res.body.should.have.property('username')
+                res.body.should.have.property('log')
+                res.body.should.have.property('count')
+                res.body._id.should.equal(firstUserId)
+                res.body.username.should.equal(firstUsername)
+                res.body.log.should.be.an('array')
+                res.body.log.should.deep.equal(firstUserFullLog.slice(1, 2))
+                res.body.count.should.equal(1)
+
+                done()
+            })
+        })
     })
 
 })
